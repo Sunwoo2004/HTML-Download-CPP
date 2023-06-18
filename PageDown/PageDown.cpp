8251b53eab320463d3389f99dab89ff51a5bd5f2
Admin <chltjsdn6653@naver.com>
2023-06-18 19:45:34 +0900

    extractElement 함수에서 오버플로우 나니까 고쳐 쓰거나, 추후에 고칠듯;

--- PageDown/PageDown.cpp
+++ PageDown/PageDown.cpp
@@ -1,20 +1,49 @@
 ﻿#include <iostream>
 #include <Windows.h>
 #include <WinInet.h>
+#include <fstream>
 #pragma comment(lib,"Wininet.lib")
+#include <gumbo.h>
+#ifdef _DEBUG
+#pragma comment(lib,"gumboD.lib")
+#else
+#pragma comment(lib,"gumbo.lib")
+#endif
 
 using namespace std;
 
 void OnPageDownload(const char* szUrl, const char* szFileName);
-
+void extractElement(GumboNode* node, const char* tagName);
+void OnParsingPage(const char * szFileName);
 
 int main()
 {
-    OnPageDownload("https://site.com", "test.txt");
+    OnPageDownload("https://www.coupang.com/", "test.txt");
     cout << "Download OK" << endl;
+
+    Sleep(100);
+
+    OnParsingPage("test.txt");
 
     system("PAUSE");
     return 0;
+}
+
+void OnParsingPage(const char* szFileName)
+{
+    std::ifstream file(szFileName);
+    if (!file)
+    {
+        std::cerr << "Failed to open file." << std::endl;
+        return;
+    }
+
+    std::string htmlContent((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    GumboOutput* output = gumbo_parse(htmlContent.c_str());
+    extractElement(output->root, "HTML");
+
+    gumbo_destroy_output(&kGumboDefaultOptions, output);
 }
 
 void OnPageDownload(const char * szUrl, const char * szFileName)
@@ -38,4 +67,21 @@ void OnPageDownload(const char * szUrl, const char * szFileName)
 
     InternetCloseHandle(hURL);
     InternetCloseHandle(hSession);
+}
+
+void extractElement(GumboNode* node, const char* tagName)
+{
+    if (node->type == GUMBO_NODE_ELEMENT)
+    {
+        GumboElement* element = &node->v.element;
+        if (strcmp((const char *)element->tag, tagName) == 0)
+        {
+            std::cout << "Tag : " << element->original_tag.data << std::endl;
+        }
+    }
+    GumboVector* children = &node->v.element.children;
+    for (unsigned int i = 0; i < children->length; ++i)
+    {
+        extractElement(static_cast<GumboNode*>(children->data[i]), tagName);
+    }
 }
